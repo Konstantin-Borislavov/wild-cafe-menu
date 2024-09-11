635a3ba6dff92bcf38c61f09d2de07bd316d79cb
Konstantin-Borislavov <konstantinborislavovc@gmail.com>
2024-09-11 20:04:10 +0200

    sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
-import { Toggle } from './components/Toggle'
+import { MorningBoosters } from './components/MorningBoosters'
+import { SharetoCare } from './components/ShareToCare'
+import { GutInstinct } from './components/GutInstinct'
+import { LifeInTechnicolour } from './components/LifeInTechnicolour'
+import { Diversity } from './components/Diversity'
+import { Desserts } from './components/Desserts'
 
 
 function App() {
 
   return (
     <>
-      HELLO WORLD
-      <Toggle/>
+      <MorningBoosters/>
+      <SharetoCare/>
+      <GutInstinct/>
+      <LifeInTechnicolour/>
+      <Diversity/>
+      <Desserts/>
     </>
   )
 }
---
+++ src/components/Desserts.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { Toggle } from './Toggle';
+import menu from '../menu.json';
+
+export const Desserts = () => {
+    const [desserts, setdesserts] = useState([]);
+
+    useEffect(() => {
+        setdesserts(menu.menu['Desserts']);
+    }, []);
+
+    return (
+        <Toggle>
+            <section className='desserts'>
+                {desserts && desserts.map((item, index) => (
+                    <div className='food' key={index}>
+                        <h3>{item.title + " -" + item.price} </h3>
+
+                        <div>
+                            <img src={item.image} loading='lazy' alt={item.title} />
+                        </div>
+
+                        <p>{item.descriptionEs}</p>
+                        <i>{item.descriptionEn}</i>
+
+                    </div>
+                ))}
+            </section>
+        </Toggle>
+    );
+};
