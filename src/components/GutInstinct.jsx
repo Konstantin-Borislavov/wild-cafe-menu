61aaffe32b222fbefc082ec3375cacc8899e37d7
Konstantin-Borislavov <konstantinborislavovc@gmail.com>
2024-09-12 20:54:08 +0200

    details-1

--- src/components/GutInstinct.jsx
+++ src/components/GutInstinct.jsx
@@ -25,6 +25,23 @@ export const GutInstinct = () => {
 
                     </div>
                 ))}
+
+                <div className='extras'>
+
+                    <h3>EXTRAS</h3>
+                    <br />
+                    <p>Tofu orgánico · <i>Organic tofu&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4.00€</i></p>
+                    <br />
+                    <p>Langostinos · <i>Praws &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;5.50€</i></p>
+                    <br />
+                    <p>Salmón ahumado · <i>Smoked salmon&nbsp;&nbsp;&nbsp;&nbsp;4.50€</i></p>
+                    <br />
+                    <p>Salmón marinado · <i>Poke salmon&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4.50€</i></p>
+                    <br />
+                    <p>Salmón salteado · <i>Sautéed salmon&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4.50€</i></p>
+                    <br />
+
+                </div>
             </section>
         </Toggle>
     );
