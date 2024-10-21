08d922b2323a2e564c9c281ae1e838b7a6b1850b
Konstantin Borislavov <konstantin@macbook-pro-de-konstantin.home>
2024-10-21 22:38:21 +0200

    details-4

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,45 +1,41 @@
-import React, { useState, useRef, useEffect } from 'react';
-import '../css/toggle.css';
+import React, { useState, useRef, useEffect } from "react";
+import "../css/toggle.css";
 
 export const Toggle = ({ title, children }) => {
   const [modal, setModal] = useState(false);
-  const [height, setHeight] = useState(0);
-  const modalContentRef = useRef(null);
+  const [showModal, setShowModal] = useState(false);
 
   const toggleModal = () => {
-    setModal(!modal);
+    window.scrollBy(0, -10);
+    if (modal) {
+      setModal(false);
+      setTimeout(() => {
+        setShowModal(false);
+      }, 300);
+    } else {
+      setShowModal(true);
+      setTimeout(() => {
+        setModal(true);
+      }, 10);
+    }
   };
-
-  useEffect(() => {
-    if (modal && modalContentRef.current) {
-      const contentHeight = modalContentRef.current.scrollHeight;
-      setHeight(contentHeight);
-    }
-  }, [modal]);
-
-  useEffect(() => {
-    if (modalContentRef.current) {
-      modalContentRef.current.style.setProperty('--height', `${height}px`);
-    }
-  }, [height]);
 
   return (
     <div>
-      <h1 className='btn-modal' onClick={toggleModal}>
+      <h1 className="btn-modal" onClick={toggleModal}>
         {title}
       </h1>
 
-      <div ref={modalContentRef} className={`modal-content ${modal ? 'open' : 'close'}`}>
-        {children}
-        <button onClick={toggleModal} className='close-button'>
-          <img
-            src={'/close-button.png'}
-            alt="Cerrar"
-          />
-        </button>
-
-      </div>
+      {showModal && (
+        <div
+          className={`modal-content ${modal ? "modal-open" : "modal-close"}`}
+        >
+          {children}
+          <button onClick={toggleModal} className="close-button">
+            <img src={"/close-button.png"} alt="Cerrar" />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
-
